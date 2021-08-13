df9b061f16d7f71b9315a12e260ba84b275e4aad
nil17593 <navgekar18@gmail.com>
2021-08-13 10:31:56 +0530

    set background scene.

---
+++ Assets/Scripts/BG Scroll Scripts/BGScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    public float scrolling_speed=0.1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
