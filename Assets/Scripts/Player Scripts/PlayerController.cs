464099d00b3128fb09255fe265813542ff5bda72
nil17593 <navgekar18@gmail.com>
2021-08-13 11:39:18 +0530

    player movement added

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed;
+    [SerializeField] private float min_Y, max_Y;
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y += speed * Time.deltaTime;
+            
+
+            if (temp.y > max_Y)
+            {
+                temp.y = max_Y;
+            }
+
+            transform.position = temp;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y -= speed * Time.deltaTime;
+            
+
+            if (temp.y < min_Y)
+            {
+                temp.y = min_Y;
+            }
+
+            transform.position = temp;
+        }
     }
 }
