b48dbb52c77b51bc531876fc0a7291eede228642
nil17593 <navgekar18@gmail.com>
2021-08-24 18:38:57 +0530

    lobby UI added.

---
+++ Assets/LobbyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    [SerializeField] private Button play;
+    [SerializeField] private Button quit;
+   
+    private void Awake()
+    {
+        play.onClick.AddListener(LoadGame);
+        quit = GetComponent<Button>();
+    }
+
+    void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
